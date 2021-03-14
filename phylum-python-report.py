b1b855926a7909555dddbdf3d52f5b8f8592f178
pmorgan <pmorgan@jumptrading.com>
2021-03-14 13:51:37 -0700

    initial setup

---
+++ phylum-python-report.py
@@ -0,0 +1,13 @@
+import os
+import sys
+from rich.table import Table
+from rich.layout import Layout
+from rich.console import Console
+from subprocess import Call
+
+def main(argc,argv):
+
+
+
+if __name__ == "__main__":
+    main(len(sys.argv),sys.argv)
